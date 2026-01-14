79cb5eb87dd7538d3c6e42ba2905ae315d12508a
yuna <yuna5470@naver.com>
2026-01-14 21:47:05 +0900

    feat: add sample LDraw car model and update viewer/pages

--- frontend/src/pages/KidsPage/KidsPage.tsx
+++ frontend/src/pages/KidsPage/KidsPage.tsx
@@ -1,6 +1,16 @@
+import kidBg from "../../assets/kid_bg.png";
+import "./KidsPage.css";
+
 export default function KidsPage() {
   return (
-    <div style={{ padding: 24 }}>
+    <div
+      className="kidsPage"
+      style={{
+        backgroundImage: `url(${kidBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <h1>키즈 모드</h1>
       <p>여기에 키즈 모드 UI 넣기</p>
     </div>
