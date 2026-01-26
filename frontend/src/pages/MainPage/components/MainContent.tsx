87483a1c3a63baeaf7a102beb8f91e981ae04c59
yuna <yuna5470@naver.com>
2026-01-26 10:15:44 +0900

    fix: 안 쓰는 컴포넌트 삭제

--- frontend/src/pages/MainPage/components/MainContent.tsx
+++
@@ -1,9 +0,0 @@
-import ModeSplit from "./ModeSplit/ModeSplit";
-
-export default function MainContent() {
-  return (
-    <main style={{ flex: 1, minHeight: 0 }}>
-      <ModeSplit />
-    </main>
-  );
-}
