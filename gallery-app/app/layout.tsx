1e0237b0db6e5419d830296e96e63f5519d6a4bc
kim-minJ <khwhj3577@gmail.com>
2026-02-12 09:33:59 +0900

    fix:독커_G4추가

--- gallery-app/app/layout.tsx
+++ gallery-app/app/layout.tsx
@@ -59,14 +59,16 @@ export default function RootLayout({
                 />
             </head>
             <body>
-                <noscript>
-                    <iframe
-                        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                        height="0"
-                        width="0"
-                        style={{ display: 'none', visibility: 'hidden' }}
-                    />
-                </noscript>
+                {GTM_ID && (
+                    <noscript>
+                        <iframe
+                            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        />
+                    </noscript>
+                )}
                 <LanguageProvider>
                     <AuthProvider>
                         <GoogleAnalytics />
@@ -75,30 +77,36 @@ export default function RootLayout({
                         </LayoutContent>
                     </AuthProvider>
                 </LanguageProvider>
-                <Script
-                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                    strategy="afterInteractive"
-                />
-                <Script id="google-analytics" strategy="afterInteractive">
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        window.gtag = function(){window.dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', '${GA_TRACKING_ID}', {
-                            page_path: window.location.pathname,
-                            debug_mode: true,
-                        });
-                    `}
-                </Script>
-                <Script id="google-tag-manager" strategy="afterInteractive">
-                    {`
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','${GTM_ID}');
-                    `}
-                </Script>
+                {GA_TRACKING_ID && (
+                    <>
+                        <Script
+                            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                            strategy="afterInteractive"
+                        />
+                        <Script id="google-analytics" strategy="afterInteractive">
+                            {`
+                                window.dataLayer = window.dataLayer || [];
+                                window.gtag = function(){window.dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${GA_TRACKING_ID}', {
+                                    page_path: window.location.pathname,
+                                    debug_mode: true,
+                                });
+                            `}
+                        </Script>
+                    </>
+                )}
+                {GTM_ID && (
+                    <Script id="google-tag-manager" strategy="afterInteractive">
+                        {`
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','${GTM_ID}');
+                        `}
+                    </Script>
+                )}
                 {/* Google Pay */}
                 <Script src="https://pay.google.com/gp/p/js/pay.js" strategy="afterInteractive" />
             </body>
